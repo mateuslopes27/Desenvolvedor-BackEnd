e4e2b03f5f14cf13a86fd5f60d97cc8bb6c038e8
mateuslopes27 <mateus.lopcosta@gmail.com>
2026-07-17 21:52:53 -0300

    Finalizado Aula 1 Prof-Cesar Dia: 17/07/2026

---
+++ LogicaDeProgramacaoEAlgoritmos/SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+// Escrever um em cada linha
+WriteLine("SENAI Americana!");
+WriteLine("Curso DEV BackEND");
+
+// Escrever na mesma linha
+Write("Espanha");
+Write("Espanha");
+
+// Sumir com as outras info, o sistema espera vc clicar alguma tecla
+ReadKey();
+
+// Dar valor a algo, igual ao VAR do JS
+int numero = 10;
+double flutuante = 7.77; // Para numeros quebrados
+string palavra = "SENAI"; // Para palavras ""
+char caracter = 'S'; // Para letras ''
+
+// Verdadeiro ou falso
+bool logica1 = false;
+bool logica2 = true;  
+
