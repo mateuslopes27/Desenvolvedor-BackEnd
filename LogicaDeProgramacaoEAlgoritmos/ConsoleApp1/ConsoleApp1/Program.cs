e63c1b33a7f10d3db28d08b41bb8d15cff0a209a
mateuslopes27 <mateus.lopcosta@gmail.com>
2026-07-20 21:56:29 -0300

    Aula 2 | Declaração de variaveis, entrada, processamento e saída de dados.

---
+++ LogicaDeProgramacaoEAlgoritmos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Declaração de variaveis
+double largura;
+double comprimento;
+double area;
+double valor;
+double preco;
+
+//Entrada de dados
+Console.WriteLine("Digite a largura do terreno: ");
+largura = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o comprimento do terreno: ");
+comprimento = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor do metro quadrado do terreno: ");
+valor = double.Parse(Console.ReadLine());
+
+//Processamento de dados
+area = largura * comprimento;
+preco = area * valor;
+
+//Saida de dados
+Console.WriteLine($"Área do terreno: {area:n}\nPreço do terreno: R${preco:n}");
---
+++ LogicaDeProgramacaoEAlgoritmos/ConsoleApp1/ExercicioRetangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿//Criação das variáveis
+using System.Security.AccessControl;
+double basee;
+double altura;
+double area;
+double perimetro;
+double diagonal;
+
+//Entrada de dados
+Console.Write("Digite a medida da base: ");
+basee = double.Parse(Console.ReadLine());
+Console.Write("Digite a medida da altura: ");
+altura = double.Parse(Console.ReadLine());
+
+//Processamento dos dados
+area = basee * altura;
+perimetro = (basee*2) + (altura*2);
+diagonal = Math.Sqrt(Math.Pow(basee, 2) + Math.Pow(altura, 2));
+
+//Saída de dados
+Console.WriteLine($"Área do retangulo: {area:f4}\nPerimetro do retangulo: {perimetro:f4}\nDiadonal do triangulo: {diagonal:f4}");
