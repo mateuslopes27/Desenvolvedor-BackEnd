9f0b26466d69c1ed7bbbfd7829c53e882550af89
mateuslopes27 <mateus.lopcosta@gmail.com>
2026-07-22 21:57:18 -0300

    Aula 3 | Medidas e Operadores 22/07/26

---
+++ LogicaDeProgramacaoEAlgoritmos/Aula03/Operadores/Medidas/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+try
+{
+    //Declaração de variáveis
+    double a, b, c;
+    double area_q, area_t, area_tp;
+
+    //Entrada de dados
+    Write("Digite o valor de A: ");
+    a = double.Parse(ReadLine());
+    Write("Digite o valor de B: ");
+    b = double.Parse(ReadLine());
+    Write("Digite o valor de C: ");
+    c = double.Parse(ReadLine());
+
+    //Processamento de dados
+    area_q = Math.Pow(a, 2);
+    area_t = (a * b) / 2;
+    area_tp = ((a + b) * c) / 2;
+
+    //Saída de dados
+    WriteLine($"A área do quadrado é: {area_q:f4}\nA área do triangula é: {area_t:f4}\nA área do trapezio é: {area_tp:f4}");
+}
+catch {
+    WriteLine("Digite um valor valido!");
+}
+finally
+{
+    WriteLine("Encerrando programa...");
+}
+
+
+
---
+++ LogicaDeProgramacaoEAlgoritmos/Aula03/Operadores/Operadores/Exercicio01.cs
@@ -0,0 +1,46 @@
+﻿
+//Criação das variáveis
+int numero1;
+int numero2;
+int numero3;
+
+int logica1;
+int logica2;
+int logica3;
+int logica4;
+
+int pergunta1;
+int pergunta2;
+int pergunta3;
+int pergunta4;
+
+
+
+//Entrada de dados
+Console.WriteLine("Digite o primeiro valor: ");
+numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo valor: ");
+numero2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o terceiro valor: ");
+numero3 = int.Parse(Console.ReadLine());
+
+//Processamento de dados
+pergunta1 = numero1 == numero2;
+pergunta2 = numero1 != numero2;
+pergunta3 = numero2 > numero1;
+pergunta4 = numero2 <= numero3;
+
+logica1 = pergunta1 & pergunta3;
+logica2 = pergunta2 | pergunta4;
+logica3 = !pergunta1;
+
+//Processamento de dados e saída de dados
+Console.WriteLine("O primeiro valor é igual ao terceiro valor?R: " + (numero1 == numero3 ? "Verdadeiro" : "Falso"));
+Console.WriteLine("O primeiro valor é diferente do segundo valor?R: " + (numero1 != numero2 ? "Verdadeiro" : "Falso"));
+Console.WriteLine("O primeiro valor é maior que o primeiro valor?R: " + (numero1 < numero2 ? "Verdadeiro" : "Falso"));
+Console.WriteLine("O primeiro valor é menor ou igual que o terceiro valor?R: " + (numero2 <= numero3 ? "Verdadeiro" : "Falso"));
+Console.WriteLine("--Lógicas--");
+Console.WriteLine($"Se a pergunta 1 e perguta 3 são verdadeiras. R; {(logica1 ? "Verdadeira" : "Falso")}");
+Console.WriteLine($"Se a pergunta 1 e perguta 3 são verdadeiras. R; {(logica2 ? "Verdadeira" : "Falso")}");
+Console.WriteLine($"Se a pergunta 1 e perguta 3 são verdadeiras. R; {(logica3 ? "Verdadeira" : "Falso")}");
+
